255dc1374b675e0951ef1186506cec09110211fd
Victor Li <vli30@my.bcit.ca>
2021-05-17 23:00:02 -0700

    Routed pages

--- Assignment02/app.js
+++ Assignment02/app.js
@@ -10,16 +10,24 @@ app.get("/", (req, res)=> res.render("pages/index"));
 
 app.get("/myForm", (req, res) => res.render("pages/myForm"));
 
-app.post("-something-", (req, res) => {
-    // Add your implementation here 
+app.post("/myForm", (req, res) => {
+    if (req.body.items) {
+        let items = req.body.items.split(",");  // items is an array of user inputs separated by commas
+        res.render("pages/result", {items: items});  // render the page "pages/result" with the json "items"
+    } else {
+        res.send("No items found.");
+    }
 });
 
-app.get("-something-", (req, res) => {
-    // Add your implementation here
+app.get("/myListQueryString", (req, res) => {
+    let items = [req.query["item1"], req.query["item2"]];  // look for "item1" and "item2" in the URL
+    res.render("pages/result", {items: items}); // render the page "pages/result" with the json "items"
 });
 
-app.get("-something-", (req, res) => {
-    // Add your implementation here
+
+app.get("/myList/:item1/:item2", (req, res) => {
+    let items = [req.params.item1, req.params.item2]; // look for "item1" and "item2" as a param in the URL
+    res.render("pages/result", {items: items}); // render the page "pages/result" with the json "items"
 });
 
 app.listen(3000);
